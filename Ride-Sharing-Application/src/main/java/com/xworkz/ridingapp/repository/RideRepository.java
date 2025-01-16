9ce66393aa9b82a2e8df4fd96b5d56743ef97266
13hoomika <bhoomika.xworkz@gmail.com>
2025-01-17 03:41:20 +0530

    Implement named query for updating in repository and service

--- Ride-Sharing-Application/src/main/java/com/xworkz/ridingapp/repository/RideRepository.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.ridingapp.repository;
-
-import com.xworkz.ridingapp.entity.RideEntity;
-
-public interface RideRepository {
-    boolean persist(RideEntity rideEntity);
-    int update(int id,Double fare);
-}
--- Ride-Sharing-Application/src/main/java/com/xworkz/ridingapp/service/RideService.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.ridingapp.service;
-
-import com.xworkz.ridingapp.entity.RideEntity;
-
-public interface RideService {
-    void persist(RideEntity rideEntity);
-    void update(int id, Double fare);
-}
